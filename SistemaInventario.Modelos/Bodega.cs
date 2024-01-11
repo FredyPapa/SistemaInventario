9374a02de37fa0381c641ff0726072142eaf988d
fpapa <fredy.luis.wolf@gmail.com>
2024-01-11 09:42:35 -0500

    Configuración del Proyecto

---
+++ SistemaInventario.Modelos/Bodega.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaInventario.Modelos
+{
+    public class Bodega
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Nombre es Requerido")]
+        [MaxLength(60, ErrorMessage = "El nombre debe tener como máximo 60 caracteres")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "Descripción es Requerido")]
+        [MaxLength(100, ErrorMessage = "La descripción debe tener como máximo 100 caracteres")]
+        public string Descripcion { get; set; }
+        [Required(ErrorMessage = "Estado es Requerido")]
+        public bool Estado { get; set; }
+    }
+}
